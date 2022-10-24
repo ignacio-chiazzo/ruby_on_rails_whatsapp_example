727f8f4947ef1115648fa572a347c485430e625b
Sahil <sahil@scaler.com>
2022-10-24 23:26:30 +0530

    feat: added pagination support v0

--- Gemfile
+++ Gemfile
@@ -77,3 +77,5 @@ group :test do
 end
 
 gem "tailwindcss-rails", "~> 2.0"
+
+gem "will_paginate", "~> 3.3"
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_01_194443) do
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "messages", force: :cascade do |t|
     t.string "message"
     t.string "sender_number"
